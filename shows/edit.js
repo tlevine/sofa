4bc67997a2adf0119a13b298ee7b43f3e5f24f72
Thomas Levine <thomas.levine@gmail.com>
2011-12-30 12:12:22 -0500

    handle quotation marks in post title

--- shows/edit.js
+++ shows/edit.js
@@ -21,7 +21,7 @@ function(doc, req) {
   
   if (doc) {
     data.doc = JSON.stringify(doc);
-    data.title = doc.title;
+    data.title = doc.title.replace("\"","&quot;");
     data.body = doc.body;
     data.tags = doc.tags.join(", ");
   } else {
